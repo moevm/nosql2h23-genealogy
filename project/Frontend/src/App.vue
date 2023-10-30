b118df370890b0efead42b67a6bee8cbfbdb80cd
NIKOLAI SOLOGUB <sologub321@mail.ru>
2023-10-30 23:27:02 +0300

    Authorization_Page: make Up

--- project/Frontend/src/App.vue
+++ project/Frontend/src/App.vue
@@ -1,6 +1,11 @@
 <template>
+  <v-app id="main">
   <router-view />
+  </v-app>
 </template>
+
+<style lang="scss">
+</style>
 
 <script setup>
   //
--- project/Frontend/src/plugins/index.js
+++ project/Frontend/src/plugins/index.js
@@ -9,9 +9,13 @@ import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
 
-export function registerPlugins (app) {
+export function registerPlugins(app) {
   app
-    .use(vuetify)
+    .use(vuetify, {
+      options: {
+        customProperties: true
+      }
+    })
     .use(router)
     .use(pinia)
 }
