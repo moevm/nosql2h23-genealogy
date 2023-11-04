113f312ef0dc5cba66e18227d76020ad1c32649b
NIKOLAI SOLOGUB <sologub321@mail.ru>
2023-11-05 02:08:18 +0300

    Profile_Page: make up

---
+++ project/Frontend/src/components/UI/MainNavigation.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-toolbar
+    class="gray-background-color"
+    elevation="5"
+  >
+    <v-btn
+      value="profile"
+      variant="text"
+      class="blue-color"
+      @click="$router.push('/profile')"
+    >
+      Профиль
+    </v-btn>
+
+    <v-btn
+      value="myTree"
+      variant="text"
+      class="blue-color"
+    >
+      Моё дерево
+    </v-btn>
+
+    <v-btn
+      value="otherTrees"
+      variant="text"
+      class="blue-color"
+    >
+      Другие родословные
+    </v-btn>
+    <v-btn
+      value="Statistic"
+      variant="text"
+      class="blue-color"
+    >
+      Статистика
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<style scoped lang="scss">
+@import "src/styles/styles";
+</style>
+
+<script>
+export default {
+  name: "mainNavigation",
+  setup() {
+
+  }
+}
+</script>
