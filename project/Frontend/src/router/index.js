fd754bbca121248324d3f8c33ecaa9c0a779b37b
AntonSeredenkov <seredenkov.anton2003@gmail.com>
2023-11-07 21:01:33 +0300

    Statistics_Page: make up

--- project/Frontend/src/router/index.js
+++ project/Frontend/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AuthorizationPage from "@/components/AuthorizationPage.vue";
 import RegistrationPage from "@/components/RegistrationPage.vue";
 import ProfilePage from "@/components/ProfilePage.vue";
+import StatisticsPage from "@/components/StaticsPage.vue"
 
 const routes = [
   {
@@ -16,8 +17,11 @@ const routes = [
   {
     path: '/profile',
     component: ProfilePage
+  },
+  {
+    path: '/statistics',
+    component: StaticsPage
   }
-
 ]
 
 const router = createRouter({
