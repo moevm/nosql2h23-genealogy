752641555a855d12261901f9ffab33d596686b30
NIKOLAI SOLOGUB <sologub321@mail.ru>
2023-12-18 17:47:25 +0300

    fieldsInTable: added

--- project/Frontend/src/components/TreePage.vue
+++ project/Frontend/src/components/TreePage.vue
@@ -41,37 +41,140 @@
       >
     </v-row>
     <v-row class="mt-10">
-    <v-text-field
-      v-model="search"
-      label="Search"
-      prepend-inner-icon="mdi-magnify"
-      single-line
-      variant="outlined"
-      hide-details
-    />
+      <p>Поиск по имени:</p>
+    </v-row>
+
+    <v-row class="mt-5">
+      <v-text-field
+        v-model="nameSearch"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        single-line
+        variant="outlined"
+        hide-details
+      />
+    </v-row>
+    <v-row class="mt-5">
+      <p>Поиск по отчеству:</p>
+    </v-row>
+    <v-row class="mt-5">
+      <v-text-field
+        v-model="patronymicSearch"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        single-line
+        variant="outlined"
+        hide-details
+      />
+    </v-row>
+    <v-row class="mt-5">
+      <p>Поиск по фамилии:</p>
+    </v-row>
+    <v-row class="mt-5">
+      <v-text-field
+        v-model="surnameSearch"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        single-line
+        variant="outlined"
+        hide-details
+      />
+    </v-row>
+    <v-row class="mt-5">
+      <p>Поиск по полу:</p>
+    </v-row>
+    <v-row class="mt-5">
+      <v-text-field
+        v-model="genderSearch"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        single-line
+        variant="outlined"
+        hide-details
+      />
+    </v-row>
+    <v-row class="mt-5">
+      <p>Поиск по дате рождения:</p>
+    </v-row>
+    <v-row class="mt-5">
+      <v-col cols="1"
+             class="mt-4">
+        От:
+      </v-col>
+      <v-col cols="2">
+      <v-text-field
+        v-model="startBirth"
+        single-line
+        variant="outlined"
+        type="date"
+        hide-details
+      />
+      </v-col>
+      <v-col cols="1" class="mt-4">
+        До:
+      </v-col>
+      <v-col cols="2">
+        <v-text-field
+          v-model="endBirth"
+          single-line
+          variant="outlined"
+          type="date"
+          hide-details
+        />
+      </v-col>
+    </v-row>
+    <v-row class="mt-5">
+      <p>Поиск по дате смерти:</p>
+    </v-row>
+    <v-row class="mt-5">
+      <v-col cols="1"
+             class="mt-4">
+        От:
+      </v-col>
+      <v-col cols="2">
+        <v-text-field
+          v-model="startDeath"
+          single-line
+          variant="outlined"
+          type="date"
+          hide-details
+        />
+      </v-col>
+      <v-col cols="1" class="mt-4">
+        До:
+      </v-col>
+      <v-col cols="2">
+        <v-text-field
+          v-model="endDeath"
+          single-line
+          variant="outlined"
+          type="date"
+          hide-details
+        />
+      </v-col>
+    </v-row>
+    <v-row class="mt-5">
+      <p>Поиск по родству:</p>
+    </v-row>
+    <v-row class="mt-5">
+      <v-text-field
+        v-model="relationSearch"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        single-line
+        variant="outlined"
+        hide-details
+      />
     </v-row>
     <v-row>
       <v-container class="gray-background-color mt-5">
         <v-radio-group column v-model="selectedSearch">
           <v-row>
             <v-col>
-              <p>Поиск по полям узла:</p>
-            </v-col>
-            <v-col>
               <p>Поиск по родству:</p>
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="2">
-          <v-radio label="Имя" value="name"></v-radio>
-          <v-radio label="Отчество" value="patronymic"></v-radio>
-          <v-radio label="Фамилия" value="surname"></v-radio>
-            </v-col>
-            <v-col>
-          <v-radio label="Пол" value="gender"></v-radio>
-          <v-radio label="Дата рождения" value="dateOfBirth"></v-radio>
-          <v-radio label="Дата смерти" value="dateOfDeath"></v-radio>
-            </v-col>
             <v-col cols="2">
               <v-radio label="Сын" value="Сын"></v-radio>
               <v-radio label="Дочь" value="Дочь"></v-radio>
@@ -93,19 +196,23 @@
       v-model:items-per-page="itemsPerPage"
       :headers="headers"
       :items="filteredItems"
-      :search="search"
       class="elevation-1 mt-10"
     >
       <template v-slot:item="row">
         <tr>
-          <td>{{ row.item.information }}</td>
+          <td>{{ row.item.name }}</td>
+          <td>{{ row.item.patronymic }}</td>
+          <td>{{ row.item.surname }}</td>
+          <td>{{ row.item.gender }}</td>
+          <td>{{ row.item.dateOfBirth }}</td>
+          <td>{{ row.item.dateOfDeath }}</td>
           <td>{{ row.item.relationship }}</td>
           <td v-if="row.item.nodeId !== userId">
             <v-btn
-            :node-id="row.item.nodeId"
-            @click="addOrChangeNode(row.item.nodeId)">
+              :node-id="row.item.nodeId"
+              @click="addOrChangeNode(row.item.nodeId)">
               <v-icon icon="mdi-pencil"/>
-              
+
             </v-btn>
             <v-btn>
               <v-icon icon="mdi-close"/>
@@ -132,7 +239,7 @@
 import MainNavigation from "@/components/UI/MainNavigation.vue";
 import {computed, onMounted, ref} from "vue";
 import {useAppStore} from "@/store/app";
-import {generateDate, generateInfo} from "../../methods/informationCreator"
+import {dateToObjectDate, generateDate, generateInfo} from "../../methods/informationCreator"
 import DeleteDialog from "@/components/UI/DeleteDialog.vue";
 import {useRouter} from 'vue-router'
 
@@ -146,7 +253,15 @@ export default {
     const router = useRouter()
     const xhr = new XMLHttpRequest();
     const uploader = ref(null)
-    const search = ref("")
+    const nameSearch = ref("")
+    const patronymicSearch = ref("")
+    const surnameSearch = ref("")
+    const genderSearch = ref("")
+    const relationSearch = ref("")
+    const startBirth = ref("")
+    const endBirth = ref("")
+    const startDeath = ref("")
+    const endDeath= ref("")
     const selectedFile = ref(null)
     const itemsPerPage = 5
     const store = useAppStore()
@@ -154,7 +269,7 @@ export default {
     const treeInfo = ref([])
     const tableDict = ref(new Map())
     const dialog = ref(false)
-    const selectedSearch = ref("name")
+    const selectedSearch = ref("Сын")
     onMounted(async () => {
       await getTreeFromDb()
     })
@@ -203,7 +318,7 @@ export default {
           } else {
             relationship.push(`Супруг: ${fullName}`)
           }
-        }else if (relative.type === 'BROTHER' || relative.type === 'SISTER') {
+        } else if (relative.type === 'BROTHER' || relative.type === 'SISTER') {
           if (gender === 'Ж') {
             relationship.push(`Сестра: ${fullName}`)
           } else {
@@ -222,8 +337,43 @@ export default {
       return true; // Для остальных столбцов всегда возвращаем true
     }
     const headers = [
-      {title: 'Информация', align: 'center', key: 'information',value: 'information', filter: value => customFilter('information', value)},
-      {title: 'Родство', align: 'center', key: 'relationship',value: 'relationship', filter: value => customFilter('relationship', value)},
+      {title: 'Имя', align: 'center', key: 'name', value: 'name', filter: value => customFilter('name', value)},
+      {
+        title: 'Отчество',
+        align: 'center',
+        key: 'patronymic',
+        value: 'patronymic',
+        filter: value => customFilter('patronymic', value)
+      },
+      {
+        title: 'Фамилия',
+        align: 'center',
+        key: 'surname',
+        value: 'surname',
+        filter: value => customFilter('surname', value)
+      },
+      {title: 'Пол', align: 'center', key: 'gender', value: 'gender', filter: value => customFilter('gender', value)},
+      {
+        title: 'Дата рождения',
+        align: 'center',
+        key: 'dateOfBirth',
+        value: 'dateOfBirth',
+        filter: value => customFilter('dateOfBirth', value)
+      },
+      {
+        title: 'Дата смерти',
+        align: 'center',
+        key: 'dateOfDeath',
+        value: 'dateOfDeath',
+        filter: value => customFilter('dateOfDeath', value)
+      },
+      {
+        title: 'Родство',
+        align: 'center',
+        key: 'relationship',
+        value: 'relationship',
+        filter: value => customFilter('relationship', value)
+      },
       {title: '', align: 'center', key: 'btn'},
     ]
 
@@ -236,8 +386,8 @@ export default {
       window.open(`http://${store.domain}:${store.serverPort}/ExportData/${store.userId}`, 'database.pdf');
     }
 
-    const sendData = async (e) =>{
-      const res = await fetch(`http://${store.domain}:${store.serverPort}/ImportData/${store.userId}`,{
+    const sendData = async (e) => {
+      const res = await fetch(`http://${store.domain}:${store.serverPort}/ImportData/${store.userId}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=utf-8'
@@ -260,7 +410,7 @@ export default {
       const data = {
         nodeId: nodeId
       }
-      const res = await fetch(`http://${store.domain}:${store.serverPort}/delete_node`,{
+      const res = await fetch(`http://${store.domain}:${store.serverPort}/delete_node`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=utf-8'
@@ -290,8 +440,8 @@ export default {
           surname: tableDict.value.get(el).information.properties.surname,
           patronymic: tableDict.value.get(el).information.properties.patronymic,
           gender: tableDict.value.get(el).information.properties.gender,
-          dateOfBirth: tableDict.value.get(el).information.properties.dateOfBirth,
-          dateOfDeath: tableDict.value.get(el).information.properties.dateOfDeath,
+          dateOfBirth: generateDate(tableDict.value.get(el).information.properties.dateOfBirth),
+          dateOfDeath: generateDate(tableDict.value.get(el).information.properties.dateOfDeath),
           relationship: tableDict.value.get(el).relationship,
           nodeId: el,
           visible: false,
@@ -301,59 +451,54 @@ export default {
       return tableData
     })
     const filteredItems = computed(() => {
-      if(["name","surname","patronymic","gender"].includes(selectedSearch.value)){
-        const filteredRes =  parseDataToTable.value.filter(item => item[selectedSearch.value].toLowerCase().includes(search.value.toLowerCase().trim()));
-        return filteredRes.map(item => { return {
-          information:generateInfo({
+      let filteredRes = parseDataToTable.value.filter(item => item["name"].toLowerCase().includes(nameSearch.value.toLowerCase().trim()));
+      filteredRes = filteredRes.filter(item => item["patronymic"].toLowerCase().includes(patronymicSearch.value.toLowerCase().trim()));
+      filteredRes = filteredRes.filter(item => item["surname"].toLowerCase().includes(surnameSearch.value.toLowerCase().trim()));
+      filteredRes = filteredRes.filter(item => item["gender"].toLowerCase().includes(genderSearch.value.toLowerCase().trim()));
+      if(startBirth.value !== ""){
+        filteredRes = filteredRes.filter(item => {
+          return new Date(startBirth.value) <= dateToObjectDate(item?.dateOfBirth)})
+      }
+      if(endBirth.value !== ""){
+        filteredRes = filteredRes.filter(item => dateToObjectDate(item?.dateOfBirth) <= new Date(endBirth.value));
+      }
+      if(startDeath.value !== ""){
+        filteredRes = filteredRes.filter(item => {
+          if(item?.dateOfDeath === "undefined.undefined.undefined"){
+            return false
+          }
+          return new Date(startDeath.value) <= dateToObjectDate(item?.dateOfDeath)})
+      }
+      if(endDeath.value !== ""){
+
+        filteredRes = filteredRes.filter(item => {
+          if(item?.dateOfDeath === "undefined.undefined.undefined"){
+            return false
+          }
+          return dateToObjectDate(item?.dateOfDeath) <= new Date(endDeath.value)});
+      }
+      filteredRes = filteredRes.map(item => {
+        return {
           name: item.name,
           surname: item.surname,
           patronymic: item.patronymic,
           gender: item.gender,
           dateOfBirth: item.dateOfBirth,
-          dateOfDeath: item.dateOfDeath,
-        }),
+          dateOfDeath: item.dateOfDeath === "undefined.undefined.undefined" ? "" : item.dateOfDeath,
           relationship: item.relationship,
           nodeId: item.nodeId,
           visible: item.visible,
         }
-        })
-      }
-      else if (["dateOfBirth","dateOfDeath"].includes(selectedSearch.value)){
-        const filteredRes =  parseDataToTable.value.filter(item => generateDate(item[selectedSearch.value]).toLowerCase().includes(search.value.toLowerCase().trim()));
-        return filteredRes.map(item => { return {
-          information:generateInfo({
-            name: item.name,
-            surname: item.surname,
-            patronymic: item.patronymic,
-            gender: item.gender,
-            dateOfBirth: item.dateOfBirth,
-            dateOfDeath: item.dateOfDeath,
-          }),
-          relationship: item.relationship,
-          nodeId: item.nodeId,
-          visible: item.visible,
+      })
+      if (["Сын", "Дочь", "Отец", "Мать", "Супруга", "Супруг", "Брат", "Сестра"].includes(selectedSearch.value)) {
+        if(relationSearch.value !== "") {
+          filteredRes = filteredRes.filter(item => {
+            const lowerRelationship = item.relationship.map(arrItem => arrItem.toLowerCase())
+            console.log(lowerRelationship)
+            return lowerRelationship.findIndex(lowerItem => lowerItem.includes(selectedSearch.value.toLowerCase() + ": " + relationSearch.value.toLowerCase().trim())) !== -1
+          });
         }
-        })
-      }
-      else if(["Сын","Дочь","Отец","Мать","Супруга","Супруг","Брат","Сестра"].includes(selectedSearch.value)){
-        const filteredRes = parseDataToTable.value.filter(item => {
-          const lowerRelationship = item.relationship.map(arrItem => arrItem.toLowerCase())
-          return lowerRelationship.findIndex(lowerItem => lowerItem.includes(selectedSearch.value.toLowerCase() + ": " + search.value.toLowerCase().trim())) !== -1
-        } );
-        return filteredRes.map(item => { return {
-          information:generateInfo({
-            name: item.name,
-            surname: item.surname,
-            patronymic: item.patronymic,
-            gender: item.gender,
-            dateOfBirth: item.dateOfBirth,
-            dateOfDeath: item.dateOfDeath,
-          }),
-          relationship: item.relationship,
-          nodeId: item.nodeId,
-          visible: item.visible,
-        }
-        })
+        return filteredRes
       }
       // Фильтруем только по первому столбцу
     })
@@ -365,7 +510,15 @@ export default {
       headers,
       selectedFile,
       tableDict,
-      search,
+      nameSearch,
+      patronymicSearch,
+      surnameSearch,
+      genderSearch,
+      relationSearch,
+      startBirth,
+      endBirth,
+      startDeath,
+      endDeath,
       handleFileImport,
       handleFileExport,
       onFileChanged,
